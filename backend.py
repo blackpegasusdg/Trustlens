02ba43808b538cd4c6ffd35e3938792352ae113a
Diptanu Ghosh <dipghosh28032005@gmail.com>
2026-08-17 13:00:02 +0530

    Fix CORS for deployed social demo

--- backend.py
+++ backend.py
@@ -35,6 +35,7 @@ app.add_middleware(
         "http://127.0.0.1:5174",
         "http://127.0.0.1:5175",
         "http://127.0.0.1:5176",
+	"https://trustlens-tau.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
