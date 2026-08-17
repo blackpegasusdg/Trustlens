fce863692d97353fa4ed73a43ee098c141692125
Diptanu Ghosh <dipghosh28032005@gmail.com>
2026-08-17 12:36:44 +0530

    Fix Login import case for Vercel

--- social-demo/src/App.jsx
+++ social-demo/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Login from "./components/Login";
+import Login from "./components/login";
 import Register from "./components/Register";
 import Feed from "./components/Feed";
 import "./index.css";
